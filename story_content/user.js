68e9be4f04b0c2766099fb8251d15cdec9bfc507
WPOiDeD <w.obogne@gmail.com>
2022-10-28 16:38:28 +0800

    CurvedLines

--- story_content/user.js
+++ story_content/user.js
@@ -1,1 +1,27 @@
-// User defined code goes here
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6KnMKMW4kkB":
+        Script1();
+        break;
+      case "5kg8fgb2jVY":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  var randomnumber = Math.floor((Math.random()*99)+1);
+var player = GetPlayer();
+player.SetVar("P_Num1",randomnumber);
+}
+
+function Script2()
+{
+  var randomnumber = Math.floor((Math.random()*99)+1);
+var player = GetPlayer();
+player.SetVar("P_Num2",randomnumber);
+}
+
